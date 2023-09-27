c1ae5f796c070792e99904ee0e81e8086450925b
Dismal <44642560+trehousecomicsDev@users.noreply.github.com>
2023-09-27 16:48:58 -0500

    Update prebuilt.mk

--- vendor/build/prebuilt.mk
+++ vendor/build/prebuilt.mk
@@ -1,4 +1,5 @@
-# Copyright (C) 2020 The LineageOS Project
+#
+# Copyright (C) 2020 The Android Open-Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,6 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
 CLANG_BINUTILS := $(abspath prebuilts/clang/host/$(HOST_PREBUILT_TAG)/llvm-binutils-stable)
 LOCAL_SHARED_LIBRARIES += $(shell (basename -s .so `$(CLANG_BINUTILS)/llvm-objdump -x $(LOCAL_PATH)/$(LOCAL_SRC_FILES) 2>/dev/null |grep NEEDED` 2>/dev/null |grep -v ^NEEDED$ |tr '\n' ' '))
